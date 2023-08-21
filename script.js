3c0a19a5f5f74a2aa31ab9fcb4ff875834810d11
Grandhi Siddu <113771456+sidd9390@users.noreply.github.com>
2023-08-21 18:19:29 +0530

    first change

---
+++ script.js
@@ -0,0 +1,40 @@
+const home = document.getElementById("home");
+const about = document.getElementById("about");
+const courses = document.getElementById("courses");
+const services = document.getElementById("services");
+const contact = document.getElementById("contact");
+function fun5(){
+    home.style.borderBottomColor ="transparent";
+    about.style.borderBottomColor ="transparent";
+    courses.style.borderBottomColor ="transparent";
+    services.style.borderBottomColor ="transparent";
+    contact.style.borderBottomColor="Black";
+}
+function fun4(){
+    home.style.borderBottomColor ="transparent";
+    about.style.borderBottomColor ="transparent";
+    courses.style.borderBottomColor ="transparent";
+    services.style.borderBottomColor ="Black";
+    contact.style.borderBottomColor="transparent";
+}
+function fun3(){
+    home.style.borderBottomColor ="transparent";
+    about.style.borderBottomColor ="transparent";
+    courses.style.borderBottomColor ="Black";
+    services.style.borderBottomColor ="transparent";
+    contact.style.borderBottomColor="transparent";
+}
+function fun2(){
+    home.style.borderBottomColor ="transparent";
+    about.style.borderBottomColor ="Black";
+    courses.style.borderBottomColor ="transparent";
+    services.style.borderBottomColor ="transparent";
+    contact.style.borderBottomColor="transparent";
+}
+function fun1(){
+    home.style.borderBottomColor ="Black";
+    about.style.borderBottomColor ="transparent";
+    courses.style.borderBottomColor ="transparent";
+    services.style.borderBottomColor ="transparent";
+    contact.style.borderBottomColor="transparent";
+}
